4fe27ecb6532e0c71dccaa07e0b4ea3d03da90fe
Sergey Tormozov <web@stormozov.ru>
2024-08-29 14:06:00 +0300

    refactor: Move private methods & add _find_element

--- habr_scraper/web_scraper.py
+++ habr_scraper/web_scraper.py
@@ -6,9 +6,9 @@ from habr_scraper.fs_tools import get_absolute_path, save_data_to_json, make_dir
 
 
 class HabrWebScraper:
-    def __init__(self, url, keywords, user_stream):
-        self.domain: str = url
-        self.keywords: list = keywords
+    def __init__(self, kw, stream):
+        self.domain: str = 'https://habr.com'
+        self.keywords: list = kw
         self.response = None
         self.all_stream_urls = {
             'articles': '/ru/articles/',
@@ -16,15 +16,11 @@ class HabrWebScraper:
             'news': '/ru/news/',
             'feed': '/ru/feed/'
         }
-        self.user_stream = user_stream
+        self.user_stream = stream
 
     def __str__(self):
         return (f'Url: {self.domain}\nKeywords: {self.keywords}\n'
                 f'Response: {self.response}')
-
-    @staticmethod
-    def _get_fake_headers():
-        return Headers(browser='chrome', os='windows').generate()
 
     def send_request(self):
         self.response = requests.get(
@@ -36,61 +32,89 @@ class HabrWebScraper:
         soup = bs4.BeautifulSoup(self.response.text, features='lxml')
         return soup.findAll('article', class_='tm-articles-list__item')
 
-    @staticmethod
-    def get_title(article):
-        return article.find('h2', class_='tm-title_h2').text
+    def get_title(self, article):
+        return self._find_element(article, 'h2', 'tm-title_h2').text
 
-    @staticmethod
-    def get_datetime(article):
-        return article.find('time')['datetime']
+    def get_datetime(self, article):
+        return self._find_element(article, 'time')['datetime']
 
     def get_url(self, article):
-        article_href = article.find('a', class_='tm-title__link')['href']
+        article_href = self._find_element(
+            article, 'a', 'tm-title__link')['href']
         return f'{self.domain}{article_href}'
 
-    @staticmethod
-    def get_preview_text(article):
-        text_body = article.find('div', class_='article-formatted-body')
-        html_tag_p = text_body.find_all('p')
+    def get_preview_text(self, article):
+        text_body = self._find_element(
+            article, 'div', 'article-formatted-body')
+        html_tag_p = self._find_element(text_body, 'p', multiple=True)
         return ' '.join([el.text for el in html_tag_p])
 
-    @staticmethod
-    def get_full_article_text(url):
+    def get_full_article_text(self, url):
         response = requests.get(url)
         soup = bs4.BeautifulSoup(response.text, features='lxml')
-        article_text = soup.find('div', class_='article-formatted-body').text
-        return article_text
+        article_text = self._find_element(
+            soup, 'div', 'article-formatted-body')
+        return article_text.text
 
-    @staticmethod
-    def get_author(article):
-        return article.find('a', class_='tm-user-info__username').text
+    def get_author(self, article):
+        return self._find_element(
+            article, 'a', 'tm-user-info__username').text
 
-    @staticmethod
-    def get_prev_img(article):
-        img_element = article.find(
-            'img', class_='tm-article-snippet__lead-image'
-        )
+    def get_prev_img(self, article):
+        img_element = self._find_element(
+            article, 'img', 'tm-article-snippet__lead-image')
         return img_element['src'] if img_element else None
 
-    @staticmethod
-    def get_tags(article):
-        tags = (
-            article
-            .find_all('a', class_='tm-publication-hub__link')
+    def get_tags(self, article):
+        tags = self._find_element(
+            article, 'a',
+            'tm-publication-hub__link', True
         )
         return [el.text.replace('*', '') for el in tags]
 
-    def extract_article_data(self, article) -> dict[str, str]:
-        return {
-            'title': self.get_title(article).strip(),
-            'time': self.get_datetime(article),
-            'url': self.get_url(article),
-            'preview_text': self.get_preview_text(article).strip(),
-            'full_text': self.get_full_article_text(self.get_url(article)),
-            'author': self.get_author(article).strip(),
-            'prev_img': self.get_prev_img(article),
-            'tags': self.get_tags(article)
-        }
+    def filter_articles_by_keywords(self, articles: list[dict[str, str]]) \
+            -> list[dict[str, str]]:
+        if not self.keywords:
+            return [self._extract_article_data(article) for article in articles]
+
+        keywords_lower = [keyword.lower() for keyword in self.keywords]
+
+        return [
+            self._extract_article_data(article)
+            for article in articles
+            if self._article_matches_keywords(
+                keywords_lower, self._extract_article_data(article)
+            )
+        ]
+
+    def scrape(self) -> list[dict[str, str]]:
+        all_articles: list = self.get_articles()
+        return self.filter_articles_by_keywords(all_articles)
+
+    @staticmethod
+    def get_articles_count(articles: list[dict[str, str]]) -> int:
+        return len(articles)
+
+    @staticmethod
+    def save_to_json_file(file_name: str, articles: list[dict[str, str]]) \
+            -> None:
+        make_dir('habr_scraper_output')
+        abs_path = get_absolute_path(['habr_scraper_output', file_name])
+        save_data_to_json(articles, abs_path)
+
+    @staticmethod
+    def _get_fake_headers():
+        return Headers(browser='chrome', os='windows').generate()
+
+    @staticmethod
+    def _find_element(
+            article: bs4.element, tag: str,
+            class_: str = None, multiple: bool = False
+    ):
+        if multiple:
+            return article.find_all(tag, class_=class_)
+        else:
+            return article.find(tag, class_=class_)
 
     @staticmethod
     def _article_matches_keywords(keywords: list, article: dict[str, str]) \
@@ -107,32 +131,14 @@ class HabrWebScraper:
             for keyword in keywords
         )
 
-    def filter_articles_by_keywords(self, articles: list) \
-            -> list[dict[str, str]]:
-        if not self.keywords:
-            return [self.extract_article_data(article) for article in articles]
-
-        keywords_lower = [keyword.lower() for keyword in self.keywords]
-
-        return [
-            self.extract_article_data(article)
-            for article in articles
-            if self._article_matches_keywords(
-                keywords_lower, self.extract_article_data(article)
-            )
-        ]
-
-    def scrape(self) -> list[dict[str, str]]:
-        all_articles: list = self.get_articles()
-        return self.filter_articles_by_keywords(all_articles)
-
-    def get_articles_count(self) -> int:
-        all_articles: list = self.scrape()
-        return len(all_articles)
-
-    def save_to_json_file(self, filename: str) -> None:
-        make_dir('habr_scraper_output')
-        abs_path = get_absolute_path(['habr_scraper_output', filename])
-        save_data_to_json(self.scrape(), abs_path)
-
-
+    def _extract_article_data(self, article) -> dict[str, str]:
+        return {
+            'title': self.get_title(article).strip(),
+            'time': self.get_datetime(article),
+            'url': self.get_url(article),
+            'preview_text': self.get_preview_text(article).strip(),
+            'full_text': self.get_full_article_text(self.get_url(article)),
+            'author': self.get_author(article).strip(),
+            'prev_img': self.get_prev_img(article),
+            'tags': self.get_tags(article)
+        }
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from habr_scraper import HabrWebScraper
 
 if __name__ == '__main__':
-    DOMAIN = 'https://habr.com'
     KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-    scraper = HabrWebScraper(DOMAIN, KEYWORDS, 'articles')
+    scraper = HabrWebScraper(KEYWORDS, 'articles')
     scraper.send_request()
-    scraper.save_to_json_file('habr')
+    articles: list[dict[str, str]] = scraper.scrape()
+    scraper.save_to_json_file('habr', articles)
